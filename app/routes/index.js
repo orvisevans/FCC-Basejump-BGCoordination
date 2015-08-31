7b2e8b854a1cf7946edef3c889b3d6e81189f2ac
orvisevans <b.orvis@gmail.com>
2015-08-30 18:34:23 -0700

    Instituted server routing

---
+++ app/routes/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+var path = process.cwd();
+
+module.exports = function(app) {
+  app.route('/')
+    .get(function (req, res) {
+      res.sendFile(path + '/public/index.html');
+    });
+};
